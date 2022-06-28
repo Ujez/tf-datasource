155b82f175606d4e3c6194f8494617fb8bff9e5e
ujez <ujezujez@gmail.com>
2022-06-28 14:08:09 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -2,18 +2,16 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_ami" "latest_ubuntu" {
+  
   most_recent = true
-  owners      = ["self"]
+  owners = ["099720109477"]
 
   filter {
-    name   = "name"
-    values = ["myami-*"]
+    name = "name"
+    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+
 }
 
 resource "aws_instance" "MyFirstInstance" {
