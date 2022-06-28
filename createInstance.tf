b9e20d191636d832c31038f65ca7c83c84b37b57
ujez <ujezujez@gmail.com>
2022-06-28 12:45:52 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -1,8 +1,23 @@
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_instance" "MyFirstInstnace" {
-  ami               = lookup(var.AMIS, var.AWS_REGION)
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "MyFirstInstance" {
+  ami               = data
   instance_type     = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
 
