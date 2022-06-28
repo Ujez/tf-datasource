3253aedc3af3c3280d88a3a1cfb928402d727b99
ujez <ujezujez@gmail.com>
2022-06-28 14:22:42 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "avilable" {}
 
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["self"]
 
   filter {
     name   = "name"
