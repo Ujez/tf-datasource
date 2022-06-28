ee06022dcb151b78471e67739e17683c6e83a89a
ujez <ujezujez@gmail.com>
2022-06-28 12:07:32 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -4,7 +4,7 @@ data "aws_availability_zones" "available" {}
 resource "aws_instance" "MyFirstInstnace" {
   ami               = lookup(var.AMIS, var.AWS_REGION)
   instance_type     = "t2.micro"
-  availability_zone = data.aws_availability_zones.available.name[1]
+  availability_zone = data.aws_availability_zones.available.names[1]
 
 
   tags = {
