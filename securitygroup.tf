b1d12ec80b9deb7657fa9e00b776c95b15d27908
ujez <ujezujez@gmail.com>
2022-06-30 13:53:35 +0100

    little update

--- securitygroup.tf
+++ securitygroup.tf
@@ -13,6 +13,10 @@ resource "aws_security_group" "sg-custom_us_east" {
     cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
   }
 
+  provisioner "local-exec" {
+    command = "echo ${aws_security_group.sg-custom_us_east} >> my_secgroup_ips.txt"
+  }
+
   tags = {
     CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
     SyncToken  = data.aws_ip_ranges.us_east_ip_range.sync_token
