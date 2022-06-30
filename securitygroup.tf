b2046bd54be4289c1c6eda4d46f4228eb0df7b04
ujez <ujezujez@gmail.com>
2022-06-30 14:04:26 +0100

    little update

--- securitygroup.tf
+++ securitygroup.tf
@@ -11,10 +11,11 @@ resource "aws_security_group" "sg-custom_us_east" {
     to_port     = "443"
     protocol    = "tcp"
     cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
-  }
 
-  provisioner "local-exec" {
-    command = "echo ${data.aws_ip_ranges.us_east_ip_range.cidr_blocks} >> my_secgroup_ips.txt"
+    provisioner "local-exec" {
+      command = "echo ${cidr_blocks} >> my_secgroup_ips.txt"
+    }
+
   }
 
   tags = {
